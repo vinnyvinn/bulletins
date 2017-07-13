3b9c68dbf06788dc407537a8be99da2f297094d8
SmoDav <smodavprivate@gmail.com>
2017-07-13 18:34:33 +0300

    removing dev seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
          $this->call(UsersTableSeeder::class);
          $this->call(PassportSeeder::class);
          $this->call(IntegrationsSeeder::class);
-         $this->call(DevelopmentSeeder::class);
+//         $this->call(DevelopmentSeeder::class);
     }
 }
