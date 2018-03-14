59ea335e9eb7ec9c6b5d4db7522099d9c4064999
brickwall <root@brickwall.wizag.biz>
2018-03-14 06:09:45 +0000

    latest change server

--- config/app.php
+++ config/app.php
@@ -12,13 +12,13 @@ return [
     | any other location as required by the application or its packages.
     */
 
-    'app_name' => 'K.B. SANGHANI',
-    'name' => 'K.B. SANGHANI & SONS',
+    'app_name' => 'R.K SANGHANI',
+    'name' => 'R.K SANGHANI',
     'telephone' => 'Tel: +254202612441/2',
     'postal' => 'P.O. BOX 26239-00504',
     'email' => 'Email: info@sanghani.co.ke',
     'location' => 'Nairobi - Kenya',
-    'delivery_prefix' => 'KBS',
+    'delivery_prefix' => 'RKS',
 
     /*
     |--------------------------------------------------------------------------
